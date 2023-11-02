c6c198d97e192c74fd5b7cfa01fa86956d8bbc1b
nawabsahab16 <nawab162004@gmail.com>
2023-11-03 00:12:59 +0530

    feat: hero section 🔥😍

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -16,10 +16,10 @@ import { Link } from 'react-router-dom'
         <p> WizCart</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=> {setMenu("shop")}}><Link to='/'>Shop</Link> {menu === "shop"? <hr/>:<></>}</li>
-        <li onClick={() =>{setMenu("mens")}}><Link to='/mens'> Men</Link> {menu === "mens"? <hr/> : <></>} </li> 
-        <li onClick={() =>{setMenu("womens")}}><Link to='/womens'> Women</Link> {menu === "womens"? <hr/> : <></>}</li>
-        <li onClick={()=> {setMenu("kids")}}> <Link to='/kids'>Kids</Link> {menu === "kids"? <hr/> : <></>}</li>
+        <li onClick={()=> {setMenu("shop")}}><Link style={{textDecoration : 'none'}} to='/'>Shop</Link> {menu === "shop"? <hr/>:<></>}</li>
+        <li onClick={() =>{setMenu("mens")}}><Link style={{textDecoration : 'none'}} to='/mens'> Men</Link> {menu === "mens"? <hr/> : <></>} </li> 
+        <li onClick={() =>{setMenu("womens")}}><Link style={{textDecoration : 'none'}} to='/womens'> Women</Link> {menu === "womens"? <hr/> : <></>}</li>
+        <li onClick={()=> {setMenu("kids")}}> <Link style={{textDecoration : 'none'}} to='/kids'>Kids</Link> {menu === "kids"? <hr/> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
           <Link to='/login'><button> Login </button> </Link> 
