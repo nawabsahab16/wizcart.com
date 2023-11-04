7f14c97356d4d4e1457c0a8a34411554b9f6ca96
nawabsahab16 <nawab162004@gmail.com>
2023-11-04 21:52:25 +0530

    feat: login functionality and product view 💖👌🏻

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className="loginsignup">
+     <div className='loginsignup-container'>
+       <h1>Sign UP</h1>
+       <div className='loginsignup-fields'>
+         <input type="text" placeholder='Your Name' />
+         <input type="email" placeholder='Your Email' />
+         <input type="password" placeholder='Password'/>
+       </div>
+       <button> Continue </button>
+       <p className='loginsignup-login'> Already have an account ? <span> Login Here</span></p>
+        <div className="loginsignup-agree">
+           <input type="checkbox" name="" id="" />
+           <p> By Continuing, I agree to the terms of use & privacy policy</p>
+        </div>
+     </div>
+    </div>
   )
 }
+
+export default LoginSignup;
