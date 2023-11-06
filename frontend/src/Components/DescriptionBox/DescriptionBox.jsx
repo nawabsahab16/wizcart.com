071e96ab2ed455696da4313feb0c61514afeda65
nawabsahab16 <nawab162004@gmail.com>
2023-11-07 00:47:36 +0530

    implemented similar product feature ✅

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -5,7 +5,24 @@ const DescriptionBox = () => {
   return (
 
    <div className='descriptionbox'> 
-     
+      <div className="descriptionbox-navigator">
+       <div className='descriptionbox-nav-box'>Description</div>
+       <div className='descriptionbox-nav-box fade '> Reviews (122) </div>
+    </div>
+     <div className='descriptionbox-description'>
+        <p> WizCart.com is your premier online destination for seamless and convenient shopping
+           experiences. Our platform serves as a dynamic virtual marketplace, revolutionizing the 
+           way you buy and sell products and services over the internet we provide a user-friendly 
+           and easily accessible environment where you can explore a plethora
+           of products and services, connect with sellers, and make hassle-free
+           transactions, all from the comfort of your own space
+        </p>
+        <p>
+        Experience the unmatched convenience, extensive accessibility, and worldwide reach that
+         WizCart.com brings to your online shopping journey. Discover the future 
+         of e-commerce with us!
+        </p>
+     </div>
    </div>
   )
 }
